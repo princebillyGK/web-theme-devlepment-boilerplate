57e3e8cc6eec4a3eaaaf4888c041f2e3dd67c01a
Prince Billy Graham Karmoker <princebillygk@gmail.com>
2021-04-07 20:28:16 +0600

    typescript was bad choice for this sort of tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,49 @@
-import {dest, parallel, series, src, watch} from 'gulp'
+const {dest, parallel, series, src, watch} = require('gulp')
 
-import postcss from 'gulp-postcss'
-import * as autoprefixer from 'autoprefixer'
-import sass from 'postcss-node-sass'
-import uncss from 'postcss-uncss'
-import cssnano from 'gulp-cssnano'
+const postcss = require('gulp-postcss')
+const autoprefixer = require('autoprefixer')
+const sass = require('postcss-node-sass')
+const uncss = require('postcss-uncss')
+const cssnano = require('gulp-cssnano')
 
-import uglify from 'gulp-uglify'
-import babel from 'gulp-babel'
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
 
-import imagemin from 'gulp-imagemin'
+const imagemin = require('gulp-imagemin')
 
-import rename from 'gulp-rename'
-import del from 'delete'
+const rename = require('gulp-rename')
+const del = require('delete')
 
-import connect from 'gulp-connect'
-import {join as pathJoin} from 'path'
-import {existsSync, mkdirSync} from 'fs'
+const connect = 'gulp-connect'
+const {join: pathJoin} = require('path')
+const {existsSync, mkdirSync} = require('fs')
 
 
 const BUILD_DIR = "./build"
 
-function getPath(path: string): string {
+function getPath(path) {
     path = pathJoin(BUILD_DIR, path)
-    if (!existsSync(path)) {
-        mkdirSync(path)
-    }
     return path
 }
 
 
-async function buildHtml() {
+function buildHtml(cb) {
     console.log("building HTML...")
-    src(["/**/*.html", "!build/**/*"])
-        .pipe(dest(getPath("/")))
+    src(["./**/*.html", "!build/**/*"])
+        .pipe(dest(getPath("./")))
     console.log("build HTML files")
+    cb()
 }
 
-async function copyVendors() {
+function copyVendors(cb) {
     console.log("copying vendor files...")
     src("./vendors/**/*")
         .pipe(dest(getPath("./vendors")))
     console.log("copied vendor files")
+    cb()
 }
 
-async function buildScripts() {
+function buildScripts(cb) {
     console.log("building scripts...")
     src('./assets/scripts/**/*.js')
         .pipe(babel())
@@ -53,16 +52,17 @@ async function buildScripts() {
         .pipe(rename({extname: '.min.js'}))
         .pipe(dest(getPath("./assets/scripts")))
     console.log("build scripts")
+    cb()
 }
 
-async function buildStyles() {
+function buildStyles(cb) {
     console.log("building styles...")
-    src("./assets/styles/main.scss")
+    src("./assets/styles/*.scss")
         .pipe(postcss([
             sass(),
             autoprefixer(),
             uncss({
-                html: ['/**/*.html']
+                html: ['./**/*.html']
             })
         ]))
         .pipe(dest(getPath("./assets/styles/")))
@@ -70,33 +70,48 @@ async function buildStyles() {
         .pipe(rename({extname: ".min.css"}))
         .pipe(dest(getPath("./assets/styles/")))
     console.log("building styles finished")
+    cb()
 }
 
-async function optimizeImages() {
+function optimizeImages(cb) {
     console.log("optimizing images...")
-    src("./assets/images/**/*")
+    src( ["./assets/images/**/*.{jpg,png,svg,gif,jpeg}"]  )
         .pipe(imagemin())
-        .pipe(dest(getPath("assets/images")))
+        .pipe(dest(getPath("./assets/images")))
     console.log("image optimization finished")
+    cb()
 }
 
-export async function clean() {
+
+function clean(cb) {
     console.log("removing previous build")
     del([BUILD_DIR])
     console.log("previous builds removed")
+    cb()
 }
 
 
-async function watchForChanges() {
-    await build()
+function watchForChanges(cb) {
+    build()
     watch("./assets/scripts/**/*.js", buildScripts)
     watch(["./assets/styles/**/*.css", "./assets/styles/**/*.scss"], buildStyles)
     watch("./assets/images/**/*", optimizeImages)
     watch(["./**/*.html", "!build/**/*"], buildHtml)
     watch("./vendors/**/*", copyVendors)
+    cb()
 }
 
-export const build = async () => series(
+
+function liveBrowser() {
+    connect.server({
+        livereload: true,
+        root: "build",
+        port: 3000
+    })
+    watchForChanges()
+}
+
+exports.default = series(
     clean,
     parallel(
         copyVendors,
@@ -107,13 +122,5 @@ export const build = async () => series(
     ),
 )
 
-export async function liveBrowser() {
-    connect.server({
-        livereload: true,
-        root: "build",
-        port: 3000
-    })
-    await watchForChanges()
-}
-
-export {build as default}
+exports.dev = liveBrowser
+exports.clean = clean
